bf4a8ebcf6ce684c4265b8d85f32e40fd705112f
Shobham Rajak <shobham.rajak@gmail.com>
2022-12-09 13:21:53 +0530

    re format

---
+++ ArraysVector/SubArraySort.java
@@ -0,0 +1,11 @@
+package ArraysVector;
+
+public class SubArraySort {
+    // problem statement: find the sub array or two indices of those element in the array, which when sorted gives the sorted array as a whole
+
+    // Brute force
+    public static int[] bruteSubArraySort(int[] a) {
+        
+    } 
+    
+}
