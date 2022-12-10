65af8dfdb4d7811e1559209f716d3225eb4094e7
Shobham Rajak <shobham.rajak@gmail.com>
2022-12-10 13:07:57 +0530

    fix bug for brute subarray sort

--- ArraysVector/SubArraySort.java
+++ ArraysVector/SubArraySort.java
@@ -1,27 +1,33 @@
 package ArraysVector;
 
+import java.util.Arrays;
+
 public class SubArraySort {
-    // problem statement: find the sub array or two indices of those element in the array, which when sorted gives the sorted array as a whole
+    // problem statement: find the sub array or two indices of those element in the
+    // array, which when sorted gives the sorted array as a whole
 
     // Brute force
     public static int[] bruteSubArraySort(int[] a) {
         int[] b = a.clone();
-        int i = 0; 
+        Arrays.sort(b);
         int n = a.length;
+        int i = 0;
         int j = n - 1;
-        while (i < n && a[i] == b[i]) {
+
+        while (i < n && a[i] == b[i])
             i++;
-        }
-        while (j >= 0 && a[i] == b[i]) {
+
+        while (j >= 0 && a[i] == b[i])
             j--;
-        }
-        if(i == a.length) return new int[] {-1, -1};
 
-        return new int[] {i, j};
+        if (i == a.length)
+            return new int[] { -1, -1 };
+
+        return new int[] { i, j };
     }
 
-    // optimized solution 
-    public static int[] optimizedSubArraySort(int[] a) {
-        
-    }
+    // optimized solution
+    // public static int[] optimizedSubArraySort(int[] a) {
+
+    // }
 }
