6785bbb30deba785361752bf73606aa7c212b836
Shobham Rajak <shobham.rajak@gmail.com>
2022-12-18 19:08:07 +0530

    add string manupulation sol'n

--- StringsAndCharacters/Search.java
+++ StringsAndCharacters/Search.java
@@ -2,7 +2,29 @@ package StringsAndCharacters;
 
 public class Search {
     
-    public static boolean search() {
-        
+    // simple updating characters in a string 
+    public static StringBuilder replaceSpace(StringBuilder str) {
+        //1. calc spaces
+        int spaces = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == ' ')
+                spaces += 1;
+        }
+
+        int idx = str.length() + 2 * spaces;
+        str.ensureCapacity(idx);
+
+        for(int i = str.length() - 1; i >= 0; i--){
+            if(str.charAt(i) == ' '){
+                str.setCharAt(idx - 1, '0');
+                str.setCharAt(idx - 2, '2');
+                str.setCharAt(idx - 3, '%');
+                idx = idx - 3;
+            } else{
+                str.setCharAt(idx - 1, str.charAt(i));
+                idx--;
+            }
+        }
+        return str; 
     }
 }
