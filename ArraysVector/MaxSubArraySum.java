71b27e523fd3e4d74959311ef3b249fd56fe9f1e
Shobham Rajak <shobham.rajak@gmail.com>
2022-12-18 12:54:17 +0530

    add kadane algo

---
+++ ArraysVector/MaxSubArraySum.java
@@ -0,0 +1,16 @@
+package ArraysVector;
+
+public class MaxSubArraySum {
+    
+    // Problem statement: find the maximum subarray sum that can be formed in the given array.
+    // Kadane's Algorithm 
+    public static int kadaneAlgo(int[] arr) {
+        int curSum = 0;
+        int globalSum = 0;
+        for(int i = 0; i < arr.length; i++) {
+            curSum = Math.max(curSum, curSum + arr[i]);
+            globalSum = Math.max(globalSum, curSum);
+        }
+        return globalSum;
+    }
+}
