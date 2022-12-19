22b9e5aaae2b0e5213f98222c2909a75a2b36f06
Shobham Rajak <shobham.rajak@gmail.com>
2022-12-19 12:53:15 +0530

    add problem statement

--- StringsAndCharacters/DigitalClock.java
+++ StringsAndCharacters/DigitalClock.java
@@ -2,7 +2,7 @@ package StringsAndCharacters;
 
 public class DigitalClock {
 
-    //
+    // You are building a logic for a clock that requires you convert absolute time in minutes into a format supported by a digital clock.
     public static String convertToString(int minutes) {
         int hours = minutes / 60;
         minutes = minutes % 60;
