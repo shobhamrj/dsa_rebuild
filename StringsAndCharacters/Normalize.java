81c3e959a7a3d25a0cbfaa7afa793c90d441c9ff
Shobham Rajak <shobham.rajak@gmail.com>
2022-12-19 14:07:28 +0530

    normalize string sol'n

---
+++ StringsAndCharacters/Normalize.java
@@ -0,0 +1,40 @@
+package StringsAndCharacters;
+
+public class Normalize {
+    
+    // You are given a sentence with words separated by spaces.
+    // Your task is to, write a function that returns a copy of this sentence where all the words:
+    //  start with a capital letter
+    //  the rest of the letters are lower case
+    public static String normalize(StringBuilder sentence) {
+        StringBuilder copy = sentence;
+        int idx = 0;
+        while(idx < sentence.length()) {
+            idx = removeWhiteSpace(copy, idx);
+            idx = normalizeWord(copy, idx);
+        }
+        return copy.toString();
+    }
+
+    private static int removeWhiteSpace(StringBuilder sentenceCopy, int idx) {
+        while(idx < sentenceCopy.length() && sentenceCopy.charAt(idx) == ' ') idx++;
+        return idx;
+    }
+
+    private static int normalizeWord(StringBuilder sentenceCopy, int idx) {
+        if(idx < sentenceCopy.length() && sentenceCopy.charAt(idx) != ' ') {
+            sentenceCopy.setCharAt(idx, Character.toUpperCase(sentenceCopy.charAt(idx)));
+            idx++;
+        }
+        while (idx < sentenceCopy.length() && sentenceCopy.charAt(idx) != ' ') {
+            sentenceCopy.setCharAt(idx, Character.toLowerCase(sentenceCopy.charAt(idx)));
+            idx++;
+        }
+        return idx;
+    }
+
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder("tHIS iS mY hOUSE");
+        System.out.println(normalize(s));
+    }
+}
