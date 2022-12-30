db6e043cc13fb76d975f36255b6e952349d9331e
Shobham Rajak <shobham.rajak@gmail.com>
2022-12-30 11:49:52 +0530

    add merge sort, update left outs

---
+++ SlidingWindow/StringWindow.java
@@ -0,0 +1,15 @@
+public class StringWindow {
+    
+    // given two strings one is called s(string) and other is called p(pattern) find a substring/window 
+    // in the bigString that has smallString in it,it should be smallest substring. 
+    // (order does not matter, but frequency of smallString matters also window can have additional characters)
+    // example Input: Fizzbuzz, fuzz    Output: Fizzbu
+    public static void findSubString(String string, String pattern) {
+        // int[] FP = new int[256];
+        // int[] FS = new int[256];
+        // for(int i = 0; i < pattern.length(); i++) {
+
+        // }
+
+    }
+}
---
+++ SortingAndSearching/MergeSort.java
@@ -0,0 +1,56 @@
+package SortingAndSearching;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    
+    public static int[] mergeSort(int[] arr) {
+        if(arr.length == 1) {
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+
+    public static int[] merge(int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        int[] temp = new int[left.length + right.length];
+        while(i < left.length && j < right.length) {
+            if(left[i] < right[j]) {
+                temp[k] = left[i];
+                i++;
+                k++;
+            } else {
+                temp[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+
+        while(i < left.length) {
+            temp[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while(j < left.length) {
+            temp[k] = right[j];
+            j++;
+            k++;
+        }
+        
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 2, 0, 7, 6, 4};
+        System.out.println(Arrays.toString(mergeSort(arr)));
+    }
+}
