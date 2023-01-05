f46c99889b59aa62dbac292b31f9f0ff4817c0b4
Shobham Rajak <shobham.rajak@gmail.com>
2023-01-05 15:53:16 +0530

    fix

--- SortingAndSearching/InversionCount.java
+++ SortingAndSearching/InversionCount.java
@@ -1,5 +1,3 @@
-// package SortingAndSearching;
-
 import java.util.ArrayList;
 
 public class InversionCount {
--- SortingAndSearching/MergeSort.java
+++ SortingAndSearching/MergeSort.java
@@ -1,5 +1,3 @@
-package SortingAndSearching;
-
 import java.util.Arrays;
 
 public class MergeSort {
