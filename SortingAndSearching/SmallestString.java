22e04917073a4f4da28567d549fc689fc51b9d23
Shobham Rajak <shobham.rajak@gmail.com>
2023-01-06 13:43:31 +0530

    smallest str sol'n

---
+++ SortingAndSearching/SmallestString.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SmallestString {
+
+    // from the list of strings, concat all the string to form the smallest string in terms of lexicographical order 
+    public static void findSmallestString(String[] arr) {
+        Arrays.sort(arr, (o1, o2) -> (o1 + o2).compareTo(o2 + o1)); // merge sort approach, while comparing two elements figure out if that goes before or after an element
+        for(String s: arr)
+            System.out.print(s);
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"a", "ab", "aba"};
+        findSmallestString(arr);
+    }
+    
+}
