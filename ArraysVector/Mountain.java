b31518b6fdf7af7e2ead84d87458f52e341b5159
Shobham Rajak <shobham.rajak@gmail.com>
2023-01-29 11:57:52 +0530

    bug fix

--- ArraysVector/Mountain.java
+++ ArraysVector/Mountain.java
@@ -6,14 +6,14 @@ public class Mountain {
         int n = a.length;
         int largest = 0;
         for(int i = 1; i <= n;) {
-            if(a[i] < a[i - 1] && a[i] > a[i + 1]) {
+            if(a[i] > a[i - 1] && a[i] > a[i + 1]) {
                 int count = 1;
                 int j = i;
                 while (j >= 1 && a[j] > a[j - 1]) {
                     count ++;
                     j--;
                 }
-                while (i < n - 2 && a[i] > a[i + 1]) {
+                while (i <= n - 2 && a[i] > a[i + 1]) {
                     count++;
                     i++;
                 }
