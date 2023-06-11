ce20ee3c840c048aef163bd4cba7ad806ad1862d
Shobham Rajak <shobham.rajak@gmail.com>
2023-06-11 17:53:00 +0530

    added another method for subseq'n

--- Recursion/SubSetSumX.java
+++ Recursion/SubSetSumX.java
@@ -7,8 +7,9 @@ public class SubSetSumX {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6};
         int sum = 10;
-//        printSubsets(new ArrayList<Integer>(), arr, arr.length, 0, sum, 0);
+        printSubsets(new ArrayList<Integer>(), arr, arr.length, 0, sum, 0);
         printOneSubset(new ArrayList<Integer>(), arr, arr.length,0, sum, 0);
+        System.out.println(countSubSequence(arr, arr.length, 0,  sum, 0));
     }
 
     private static void printSubsets(ArrayList<Integer> res, int[] arr, int n, int i, int sum, int currSum) {
@@ -42,5 +43,25 @@ public class SubSetSumX {
         currSum -= arr[i];
         if(printOneSubset(res, arr, n, i + 1, sum, currSum)) return true;
         return false;
+    }
+
+    // count subsequences that sums to a num
+
+    private static int countSubSequence(int[] arr, int n, int i, int sum, int currSum) {
+        if(currSum > sum) return 0;
+        if(i == n) {
+            if(currSum == sum) {
+                return 1;
+            }
+            return 0;
+        }
+
+        currSum += arr[i];
+        int includes = countSubSequence(arr, n, i + 1, sum, currSum);
+
+        currSum -= arr[i];
+        int excludes = countSubSequence(arr, n, i + 1, sum, currSum);
+
+        return includes + excludes;
     }
 }
