d59bcf2fdc566b78ce850b2de82fc81a3250f01e
Shobham Rajak <shobham.rajak@gmail.com>
2023-06-17 13:46:40 +0530

    combination sum 2 sol'n

--- Recursion/CombinationSumTwo.java
+++ Recursion/CombinationSumTwo.java
@@ -1,5 +1,32 @@
 package Recursion;
 
+import java.util.*;
+
 // ref - https://leetcode.com/problems/combination-sum-ii/description/
 public class CombinationSumTwo {
+    static List<List<Integer>> res = new ArrayList<>();
+
+
+    private static void findCombinations(int idx, int[] arr, int target, ArrayList<Integer> ds) {
+        if(target == 0) {
+            res.add(new ArrayList<>(ds));
+            return;
+        }
+        for (int i = idx; i < arr.length; i++) {
+            if(i > idx && arr[i] == arr[i - 1]) continue;
+            if(arr[i] > target) break;
+
+            ds.add(arr[i]);
+            findCombinations(i + 1, arr, target - arr[i], ds);
+            ds.remove(ds.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,1,2,7,6,1,5};
+        int target = 8;
+        Arrays.sort(arr);
+        findCombinations(0, arr, target, new ArrayList<>());
+        System.out.println(res.toString());
+    }
 }
